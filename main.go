aa3b67aa340579764be9f53cbb1826e312290a1b
xywf221 <993651481@qq.com>
2018-01-14 14:23:34 +0800

    更新文件

--- main.go
+++ main.go
@@ -1,4 +1,8 @@
 package main
 
+import "fmt"
+
 func main() {
+	fmt.Println(Bin2hex("example hex data"))
+	fmt.Println(Hex2bin("6578616d706c65206865782064617461"))
 }
--- tool.go
+++ tool.go
@@ -1,4 +1,47 @@
 package main
 
-func main() {
+import (
+	"strconv"
+	"log"
+)
+
+func Hex2bin(hex string) (str string) {
+	if len(hex)%2 != 0 {
+		return
+	}
+	for i := 0; i < len(hex)-1; i += 2 {
+		//str += strconv.QuoteRuneToGraphic(rune(hexdec(hex[i:i+2])))
+		ascii := rune(hexdec(hex[i:i+2]))
+		str += strconv.QuoteRuneToASCII(ascii)[1:2]
+	}
+	return
+}
+
+func Bin2hex(str string) (hex string) {
+	for i := 0; i < len(str); i++ {
+		hex += dechex(CharCodeAt(str, i))
+	}
+	return hex
+}
+
+func CharCodeAt(str string, i int) int {
+	/*	value, _, _, err := strconv.UnquoteChar(CharAt(str, i), '"')
+		if err != nil {
+			log.Fatalln(err.Error())
+			return 0
+		}
+		return int(value)*/
+	return int(str[i])
+}
+
+func dechex(number int) string {
+	return strconv.FormatInt(int64(number), 16)
+}
+
+func hexdec(hex string) int64 {
+	i, err := strconv.ParseInt(hex, 16, 0)
+	if err != nil {
+		log.Println(err.Error())
+	}
+	return i
 }
